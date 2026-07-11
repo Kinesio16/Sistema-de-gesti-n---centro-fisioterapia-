ac76565fc50ba8ab0b5833222f69de17478df887
PaulArias27 <paularias2727@gmail.com>
2026-07-10 23:39:39 -0500

    PA:initialize Spring Boot project with PostgreSQL configuration

---
+++ src/main/java/com/kinesiovitality/KinesioVitalityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kinesiovitality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinesioVitalityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinesioVitalityBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinesiovitality/KinesioVitalityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinesiovitality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinesioVitalityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
